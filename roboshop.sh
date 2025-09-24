e1532783e056b3a0710a5a5082b04879ad43b2cb
yuvaraj <yuvarajreddy24@gmail.com>
2025-09-24 21:03:16 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -9,13 +9,12 @@ for instance in $@
 do
    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 
-    if [ $instance != "frontend" ]; then 
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.yuvarajreddy.fun
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
     else
-       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)  
-       RECORD_NAME="$DOMAIN_NAME" #yuvarajreddy.fun
-   
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
     fi
 
     echo $instance:"$IP"
