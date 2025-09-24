054e506c35ac470bd82588d0ecb41bd91225609d
yuvaraj <yuvarajreddy24@gmail.com>
2025-09-24 20:54:57 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -15,7 +15,7 @@ do
     else
        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)  
        RECORD_NAME="$DOMAIN_NAME" #yuvarajreddy.fun
-    else
+   
     fi
 
     echo $instance:"$IP"
