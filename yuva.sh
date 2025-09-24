b33faf744535787f229514257444b09b562310a9
yuvaraj <yuvarajreddy24@gmail.com>
2025-09-25 00:00:59 +0530

    yuva.sh

--- yuva.sh
+++ yuva.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PERSON1:"YUVA"
-PERSON2:"BHASKAR"
-PERSON3:"RAMYA"
+$PERSON1:"YUVA"
+$PERSON2:"BHASKAR"
+$PERSON3:"RAMYA"
 
 echo "$PERSON1: Hi,how are you"
 echo "$PERSON2:I'm fine,what about you"
