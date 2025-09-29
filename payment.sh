7e7ddfab15ff314ebfdc86d4d75870b0061a080f
yuvaraj <yuvarajreddy24@gmail.com>
2025-09-29 22:21:47 +0530

    .

--- payment.sh
+++ payment.sh
@@ -9,9 +9,9 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.yuvarajreddy.fun
+MONGODB_HOST=mongodb.sh.yuvarajreddy.fun
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-MYSQL_HOST=mysql.yuvarajreddy.fun
+MYSQL_HOST=mysql.sh.yuvarajreddy.fun
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
